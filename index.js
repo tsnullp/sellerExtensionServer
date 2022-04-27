7525f7a27783281c017ae0b6a87841e5164bc8e6
tsnullp <tsnullp@gmail.com>
2022-04-27 16:11:58 +0900

    aa

--- index.js
+++ index.js
@@ -189,7 +189,6 @@ app.post("/amazon/isRegister", async (req, res) => {
 app.post("/amazon/isRegisters", async (req, res) => {
   try {
     const { user, items } = req.body
-    console.log("user, items", user, items)
     const userInfo = await User.findOne({
       email: user,
     })
