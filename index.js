59f4e6f8cb8b8334a9e11c82da9d75dc1fad3963
tsnullp <tsnullp@gmail.com>
2022-07-05 07:46:08 +0900

    aa

--- index.js
+++ index.js
@@ -1046,7 +1046,7 @@ app.post("/amazon/aliText", async (req, res) => {
 app.post("/ali/cookie", async (req, res) => {
   try {
     const { xman_t } = req.body
-    if (!xman_t || !xman_t.includes("==") || xman_t.length > 100) {
+    if (!xman_t || !xman_t.includes("==") || xman_t.length < 100) {
       res.json({
         message: "fail",
       })
